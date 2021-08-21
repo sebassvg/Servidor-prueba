5f342b89f8963676473210fd7ece1236f74d87c9
sebasvg <svizcarragradill@gmail.com>
2021-08-21 11:56:18 -0500

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+// Rutas para autenticar usuarios
+const express = require('express')
+const router = express.Router();
+const { check } = require('express-validator')
+const authController = require('../controllers/authController')
+const auth = require('../middleware/auth')
+
+
+// Iniciar sesión
+// api/auth
+router.post('/',
+    authController.autenticarUsuarios
+)
+
+// Obtiene el usuario autenticado
+router.get('/', 
+    auth,
+    authController.usuarioAutenticado
+)
+
+module.exports = router
